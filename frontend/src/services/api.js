79cc02f6a25e93653496c3c61e3ec21c68a9d362
Caitlin O'Donnell <cmo21e@fsu.edu>
2026-04-28 18:16:20 -0400

    work: update api service

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const API_URL = 'http://10.0.0.79:3000/api';
+const API_URL = 'http://localhost:3000/api';
 
 const api = axios.create({
   baseURL: API_URL,
